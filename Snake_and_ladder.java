e3c839f0b748edb830206f85f1e94c9e81313b2b
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-19 11:30:15 +0530

    Added UC1 for start position as 0

---
+++ Snake_and_ladder.java
@@ -0,0 +1,7 @@
+public class Snake_and_ladder {
+    public static void main(String[] args) {
+        int start_position = 0;
+        System.out.println("Start position is : " +start_position);
+
+    }
+}
