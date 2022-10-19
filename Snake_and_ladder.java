874c1a4adaade6e499c6f4af1029550880da5842
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-19 11:40:50 +0530

    Added UC2 for Die roll

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -1,7 +1,8 @@
 public class Snake_and_ladder {
     public static void main(String[] args) {
         int start_position = 0;
+        int die_roll = (int) ((Math.random()*10)%6+1);
         System.out.println("Start position is : " +start_position);
-
+        System.out.println("Die roll number is : " +die_roll);
     }
 }
