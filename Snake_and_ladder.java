119c45b68690afc8792205be69e0ead82eac980d
mandira-05 <mandira.sn.gowda@gmail.com>
2022-10-19 13:52:33 +0530

    Added code till player reaches 100

--- Snake_and_ladder.java
+++ Snake_and_ladder.java
@@ -1,29 +1,35 @@
 public class Snake_and_ladder {
     public static void main(String[] args) {
+        int finish=100;
         int start_position = 0;
-        int die_roll = (int) ((Math.random()*10)%6+1);
-        System.out.println("Start position is : " +start_position);
-        System.out.println("Die roll number is : " +die_roll);
 
-        int check_option_play= (int) ((Math.random()*10)%3+1);
-        //System.out.println("Check option for play : " +check_option_play);
+        while (start_position<finish) {
 
-        if (check_option_play == 1 ) {
-            System.out.println("You got no move");
-            //start_position=start_position+0;
+            int die_roll = (int) ((Math.random()*10)%6+1);
+            int check_option_play = (int) ((Math.random() * 10) % 3 + 1);
+
+
+            if (check_option_play == 1) {
+                System.out.println("You got no move");
+
+            } else if (check_option_play == 2) {
+                System.out.println("You got Ladder");
+                start_position = start_position + die_roll;
+                System.out.println("You move " + die_roll + " positions forward");
+
+            } else {
+                System.out.println("You got Snake");
+                start_position = start_position - die_roll;
+                System.out.println("You move " + die_roll + " positions back");
+                if (start_position < 0 ) {
+                    System.out.println("Start Again");
+                    start_position = 0;
+                }
+            }
+
         }
-        else if (check_option_play == 2 ) {
-            System.out.println("You got Ladder");
-            start_position=start_position+die_roll;
-            System.out.println("You move " +start_position+ " positions forward");
-
-        } else {
-            System.out.println("You got Snake");
-            start_position=start_position-die_roll;
-            System.out.println("You move " +start_position+ " positions back");
-        }
-
-
+        System.out.println("Position is now: " +start_position);
     }
 }
+
 
